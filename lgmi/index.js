bce22a15dfc9453c4a70484a61f65b87eb855805
surbhi594 <71223742+surbhi594@users.noreply.github.com>
2021-07-19 19:31:19 +0530

    Task-1 done

---
+++ lgmi/index.js
@@ -0,0 +1,50 @@
+mainImg=document.getElementById("mainImg");
+
+t1=document.getElementById('t1');
+t1Src=document.getElementById('t1').src;
+ 
+t2=document.getElementById('t2');
+t2Src=document.getElementById('t2').src;
+
+t3=document.getElementById('t3');
+t3Src=document.getElementById('t3').src;
+
+t4=document.getElementById('t4');
+t4Src=document.getElementById('t4').src;
+
+t5=document.getElementById('t5');
+t5Src=document.getElementById('t5').src;
+
+t6=document.getElementById('t6');
+t6Src=document.getElementById('t6').src;
+
+t7=document.getElementById('t7');
+t7Src=document.getElementById('t7').src;
+
+t1.addEventListener("click",function(){
+  mainImg.src=t1Src
+})
+
+t2.addEventListener("click",function(){
+  mainImg.src=t2Src
+})
+
+t3.addEventListener("click",function(){
+  mainImg.src=t3Src
+})
+
+t4.addEventListener("click",function(){
+  mainImg.src=t4Src
+})
+
+t5.addEventListener("click",function(){
+  mainImg.src=t5Src
+})
+
+t6.addEventListener("click",function(){
+  mainImg.src=t6Src
+})
+
+t7.addEventListener("click",function(){
+  mainImg.src=t7Src
+})
